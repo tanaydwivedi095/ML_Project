acf01272afa0829e0c1e4afd4a98477712a55820
Tanay Dwivedi <tanaydwivedi095@gmail.com>
2025-02-26 19:49:24 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+    return requirements
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Tanay",
+    author_email="tanaydwivedi095@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+)
